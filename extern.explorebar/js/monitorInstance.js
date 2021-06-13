33cbd7bbb1ef66125656dd1906163f9c1710b624
AnesuChiodze <anesuemail@gmail.com>
2021-06-13 22:47:49 +1000

    More code clean-up

--- extern.explorebar/js/monitorInstance.js
+++ extern.explorebar/js/monitorInstance.js
@@ -5,13 +5,13 @@ let setDeskasDesktop = (winID) => {
 	console.log("setDeskasDesktop",winID);
     var exec = require('child_process').exec,
                    child;
-            child = exec('xprop -id '+winID+' -f _NET_WM_WINDOW_TYPE 32a -set _NET_WM_WINDOW_TYPE _NET_WM_WINDOW_TYPE_'+winType+' && xprop -id '+winID+' -f _MOTIF_WM_HINTS 32c -set _MOTIF_WM_HINTS 0 && xprop -id '+winID+' -remove _MOTIF_WM_HINTS');
+            child = exec(`xprop -id ${winID} -f _NET_WM_WINDOW_TYPE 32a -set _NET_WM_WINDOW_TYPE _NET_WM_WINDOW_TYPE_${winType} && xprop -id ${winID} -f _MOTIF_WM_HINTS 32c -set _MOTIF_WM_HINTS 0 && xprop -id ${winID} -remove _MOTIF_WM_HINTS`);
 }
 
 let getWindowID = (winTitle,callback) => {
     var exec = require('child_process').exec,
                    child;
-    child = exec("xdotool search --name '"+winTitle+"'",function (error, stdout, stderr) {
+    child = exec(`xdotool search --name "${winTitle}"`,function (error, stdout, stderr) {
 		if (error !== null) {
 			console.log('exec error: ' + error);
 		} else {
